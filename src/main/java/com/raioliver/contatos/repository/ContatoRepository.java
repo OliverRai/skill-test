75884fcd9c2ff1644cedce60c4ec37c04584b0b8
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-25 19:05:02 -0300

    adicionando ContatoRepository extendendo o jpa

---
+++ src/main/java/com/raioliver/contatos/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package com.raioliver.contatos.repository;
+
+import com.raioliver.contatos.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+}
